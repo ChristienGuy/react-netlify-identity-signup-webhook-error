1572e73a002391546f813eb3a9ae31b2796a6035
Chris Guy <christien.guy@gmail.com>
2019-07-23 14:56:32 +0100

    sign up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import {
+  IdentityContextProvider,
+  useIdentityContext
+} from "react-netlify-identity";
+import "./App.css";
+
+const Auth = () => {
+  const formRef = useRef();
+  const { signupUser } = useIdentityContext();
+  return (
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+      ref={formRef}
+      onSubmit={e => {
+        e.preventDefault();
+        const email = formRef.current.email.value;
+        const password = formRef.current.password.value;
+
+        signupUser(email, password).then(res => console.log(res));
+      }}
+    >
+      <label>
+        Email:
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" />
+      </label>
+      <input type="submit" value="Sign Up" />
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IdentityContextProvider url="https://react-netlify-identity-signup-webhook-error.netlify.com">
+      <Auth />
+    </IdentityContextProvider>
   );
 }
 
